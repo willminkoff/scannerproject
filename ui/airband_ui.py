5241ff352850a154da7196140f2086a1e070e9d2
willminkoff <willminkoff@gmail.com>
2026-01-06 13:51:44 +0000

    Add KHOP, KMQY, and MTEARS profiles

--- ui/airband_ui.py
+++ ui/airband_ui.py
@@ -42,7 +42,10 @@ UNITS = {
 PROFILES = [
     ("airband", "NASHVILLE", os.path.join(PROFILES_DIR, "rtl_airband_airband.conf")),
     ("tower",  "TOWER (118.600)", os.path.join(PROFILES_DIR, "rtl_airband_tower.conf")),
+    ("khop",   "KHOP (Campbell)", os.path.join(PROFILES_DIR, "rtl_airband_khop.conf")),
+    ("kmqy",   "KMQY (Smyrna)", os.path.join(PROFILES_DIR, "rtl_airband_kmqy.conf")),
     ("gmrs",   "GMRS", os.path.join(PROFILES_DIR, "rtl_airband_gmrs.conf")),
+    ("mtears", "MTEARS", os.path.join(PROFILES_DIR, "rtl_airband_mtears.conf")),
     ("wx",     "WX (162.550)", os.path.join(PROFILES_DIR, "rtl_airband_wx.conf")),
 ]
 
